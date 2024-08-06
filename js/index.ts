f24c39874107d7586e93f0d94b837f69b55d1bc0
Dimitar Mitov <dimitarmitoff@gmail.com>
2024-08-06 19:41:10 +0300

    Update index.ts

--- js/index.ts
+++ js/index.ts
@@ -48,4 +48,19 @@ export const defaultOptions: BrowserOptions = {
   autoSessionTracking: false,
   beforeSend: sendEventToRust,
   beforeBreadcrumb: sendBreadcrumbToRust,
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  integrations: [
+    Sentry.replayIntegration({
+      // Additional SDK configuration goes in here, for example:
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
 };
